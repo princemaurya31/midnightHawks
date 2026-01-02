ef731b52f544e941dc79c80cf6f282331688aa17
princemaurya31 <princemaurya9123@gmail.com>
2026-01-02 11:04:07 +0530

    Fix frontend path for Render

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path = require("path");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -16,15 +16,12 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/complaints", require("./routes/complaint"));
 
-// ✅ Frontend serve (IMPORTANT)
-app.use(express.static(path.join(__dirname, "frontend")));
+// 👇 FRONTEND SERVE
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
-// ✅ listen ALWAYS LAST
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
